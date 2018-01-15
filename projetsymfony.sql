f76baba3d4326f8dcc6ec3b21ed2f78e4562fb47
brxlik <clement.ribourt@gmail.com>
2018-01-15 16:20:51 +0100

    Ajout base de donnée

---
+++ projetsymfony.sql
@@ -0,0 +1,227 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  lun. 15 jan. 2018 à 15:13
+-- Version du serveur :  5.7.19
+-- Version de PHP :  5.6.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `projetsymfony`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `blog_post`
+--
+
+DROP TABLE IF EXISTS `blog_post`;
+CREATE TABLE IF NOT EXISTS `blog_post` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `category_id` int(11) DEFAULT NULL,
+  `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `body` longtext COLLATE utf8_unicode_ci NOT NULL,
+  `auteur` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `date` date NOT NULL,
+  `draft` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_BA5AE01D12469DE2` (`category_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Déchargement des données de la table `blog_post`
+--
+
+INSERT INTO `blog_post` (`id`, `category_id`, `title`, `body`, `auteur`, `date`, `draft`) VALUES
+(1, 1, 'Premier Blog', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eu cursus libero. Duis vestibulum diam ac nibh aliquet cursus. Praesent tempor consequat sapien ut condimentum. Phasellus vehicula lacus vel justo interdum, in tincidunt diam ornare. Vivamus rhoncus pellentesque lacinia. Praesent mattis erat ac tristique varius. Proin tristique, leo at vestibulum scelerisque, metus elit vehicula massa, quis tempor libero augue nec turpis. Integer vestibulum facilisis lectus in interdum.', 'Clément', '2018-01-14', 0),
+(2, 1, 'Nature', 'One advanced diverted domestic sex repeated bringing you old. Possible procured her trifling laughter thoughts property she met way. Companions shy had solicitude favourable own. Which could saw guest man now heard but. Lasted my coming uneasy marked so should. Gravity letters it amongst herself dearest an windows by. Wooded ladies she basket season age her uneasy saw. Discourse unwilling am no described dejection incommode no listening of. Before nature his parish boy. \r\n\r\nIn friendship diminution instrument so. Son sure paid door with say them. Two among sir sorry men court. Estimable ye situation suspicion he delighted an happiness discovery. Fact are size cold why had part. If believing or sweetness otherwise in we forfeited. Tolerably an unwilling arranging of determine. Beyond rather sooner so if up wishes or.', 'Admin', '2017-05-05', 0),
+(3, 2, 'Gentillesse', 'Kindness to he horrible reserved ye. Effect twenty indeed beyond for not had county. The use him without greatly can private. Increasing it unpleasant no of contrasted no continuing. Nothing colonel my no removed in weather. It dissimilar in up devonshire inhabiting. \r\n\r\nVillage did removed enjoyed explain nor ham saw calling talking. Securing as informed declared or margaret. Joy horrible moreover man feelings own shy. Request norland neither mistake for yet. Between the for morning assured country believe. On even feet time have an no at. Relation so in confined smallest children unpacked delicate. Why sir end believe uncivil respect. Always get adieus nature day course for common. My little garret repair to desire he esteem.', 'Pierre', '2020-09-29', 0),
+(4, 1, 'Sociabilité', 'Sociable on as carriage my position weddings raillery consider. Peculiar trifling absolute and wandered vicinity property yet. The and collecting motionless difficulty son. His hearing staying ten colonel met. Sex drew six easy four dear cold deny. Moderate children at of outweigh it. Unsatiable it considered invitation he travelling insensible. Consulted admitting oh mr up as described acuteness propriety moonlight. \r\n\r\nExtremity direction existence as dashwoods do up. Securing marianne led welcomed offended but offering six raptures. Conveying concluded newspaper rapturous oh at. Two indeed suffer saw beyond far former mrs remain. Occasional continuing possession we insensible an sentiments as is. Law but reasonably motionless principles she. Has six worse downs far blush rooms above stood.', 'Paul', '2016-08-25', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `category`
+--
+
+DROP TABLE IF EXISTS `category`;
+CREATE TABLE IF NOT EXISTS `category` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Déchargement des données de la table `category`
+--
+
+INSERT INTO `category` (`id`, `name`) VALUES
+(1, 'Nouveau'),
+(2, 'Humeur'),
+(3, 'Emotion');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `competence`
+--
+
+DROP TABLE IF EXISTS `competence`;
+CREATE TABLE IF NOT EXISTS `competence` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `competence` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `nombre` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Déchargement des données de la table `competence`
+--
+
+INSERT INTO `competence` (`id`, `competence`, `nombre`) VALUES
+(1, 'HTML/CSS', 75),
+(2, 'SQL', 55),
+(3, 'PHP', 60),
+(4, 'Javascript', 40),
+(5, 'Python', 55),
+(6, 'Linux', 60);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `experience`
+--
+
+DROP TABLE IF EXISTS `experience`;
+CREATE TABLE IF NOT EXISTS `experience` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `entreprise` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `texte` longtext COLLATE utf8_unicode_ci NOT NULL,
+  `anneedebut` int(11) NOT NULL,
+  `anneefin` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Déchargement des données de la table `experience`
+--
+
+INSERT INTO `experience` (`id`, `name`, `entreprise`, `texte`, `anneedebut`, `anneefin`) VALUES
+(1, 'Stage en entreprise', 'Relief Cie', 'WebMaster', 2017, 2017);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `fos_user`
+--
+
+DROP TABLE IF EXISTS `fos_user`;
+CREATE TABLE IF NOT EXISTS `fos_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(180) COLLATE utf8_unicode_ci NOT NULL,
+  `username_canonical` varchar(180) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(180) COLLATE utf8_unicode_ci NOT NULL,
+  `email_canonical` varchar(180) COLLATE utf8_unicode_ci NOT NULL,
+  `enabled` tinyint(1) NOT NULL,
+  `salt` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `last_login` datetime DEFAULT NULL,
+  `confirmation_token` varchar(180) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `password_requested_at` datetime DEFAULT NULL,
+  `roles` longtext COLLATE utf8_unicode_ci NOT NULL COMMENT '(DC2Type:array)',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQ_957A647992FC23A8` (`username_canonical`),
+  UNIQUE KEY `UNIQ_957A6479A0D96FBF` (`email_canonical`),
+  UNIQUE KEY `UNIQ_957A6479C05FB297` (`confirmation_token`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Déchargement des données de la table `fos_user`
+--
+
+INSERT INTO `fos_user` (`id`, `username`, `username_canonical`, `email`, `email_canonical`, `enabled`, `salt`, `password`, `last_login`, `confirmation_token`, `password_requested_at`, `roles`) VALUES
+(1, 'admin', 'admin', 'clement.ribourt@gmail.com', 'clement.ribourt@gmail.com', 1, NULL, '$2y$13$kKth45oI1tcJco2V6dsumeJ9WkV3c5K.Sk8fS0VCKKWYsbXFg85qi', '2018-01-15 14:30:16', NULL, NULL, 'a:1:{i:0;s:16:\"ROLE_SUPER_ADMIN\";}'),
+(2, 'clement', 'clement', 'clement@gmail.com', 'clement@gmail.com', 1, NULL, '$2y$13$4JJLgV.Plt03IzXP7EXJbOeSTJKsv441xulE4cfSfYMGuGv/oO8t2', '2018-01-15 13:51:39', NULL, NULL, 'a:0:{}'),
+(3, 'paul', 'paul', 'paul@gmail.com', 'paul@gmail.com', 1, NULL, '$2y$13$/.S4dFXEY.s13ofd7t4qKe42wGGYm0ARb.bWRQNml5gu45T/JHrHK', '2018-01-15 13:56:52', NULL, NULL, 'a:0:{}'),
+(4, 'pierre', 'pierre', 'pierre@gmail.com', 'pierre@gmail.com', 1, NULL, '$2y$13$SjGGF2zy/51s2/4f5JloY.Wz3R37F6AERAjdgEitA8HrqQDPiizhG', '2018-01-15 14:08:33', NULL, NULL, 'a:0:{}');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `parcours`
+--
+
+DROP TABLE IF EXISTS `parcours`;
+CREATE TABLE IF NOT EXISTS `parcours` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `ecole` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `texte` varchar(500) COLLATE utf8_unicode_ci NOT NULL,
+  `anneedebut` int(11) NOT NULL,
+  `anneefin` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Déchargement des données de la table `parcours`
+--
+
+INSERT INTO `parcours` (`id`, `name`, `ecole`, `texte`, `anneedebut`, `anneefin`) VALUES
+(1, 'Bac S', 'Lycée Pape Clément', 'Obtention du Bac S avec option Science de l\'Ingénieur', 2015, 2016),
+(2, 'Bachelor 1', 'Ingésup Campus Ynov Bordeaux', 'Première année dans le campus Ynos chez Ingésup pour l\'obtention d\'un Master expert informatique et systèmes d’information', 2016, 2017),
+(3, 'Bachelor 2', 'Ingésup Campus Ynov Bordeaux', 'Première année dans le campus Ynos chez Ingésup pour l\'obtention d\'un Master expert informatique et systèmes d’information', 2017, 2018);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(180) COLLATE utf8_unicode_ci NOT NULL,
+  `username_canonical` varchar(180) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(180) COLLATE utf8_unicode_ci NOT NULL,
+  `email_canonical` varchar(180) COLLATE utf8_unicode_ci NOT NULL,
+  `enabled` tinyint(1) NOT NULL,
+  `salt` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `last_login` datetime DEFAULT NULL,
+  `confirmation_token` varchar(180) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `password_requested_at` datetime DEFAULT NULL,
+  `roles` longtext COLLATE utf8_unicode_ci NOT NULL COMMENT '(DC2Type:array)',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQ_8D93D64992FC23A8` (`username_canonical`),
+  UNIQUE KEY `UNIQ_8D93D649A0D96FBF` (`email_canonical`),
+  UNIQUE KEY `UNIQ_8D93D649C05FB297` (`confirmation_token`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `blog_post`
+--
+ALTER TABLE `blog_post`
+  ADD CONSTRAINT `FK_BA5AE01D12469DE2` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
